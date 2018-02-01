9a951d3941b1277c8466bd839ea7583debf4c0de
mranchon <dm.ranchon@gmail.com>
2018-02-01 15:00:45 +0100

    Measure.php

--- classes/Measure.php
+++ classes/Measure.php
@@ -5,11 +5,13 @@ class Measure
     public $id;
     public $temperature;
     public $humidite;
+    public $datetime;
 
-    public function __construct($temperature, $humidite)
+    public function __construct($temperature, $humidite, $datetime)
     {
         $this->temperature = $temperature;
         $this->humidite = $humidite;
+        $this->datetime = $datetime;
     }
 }
 
