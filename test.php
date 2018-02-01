0881364645258211c96ae0803145b2ca34011d7b
mranchon <dm.ranchon@gmail.com>
2018-02-01 14:01:20 +0100

    1ere phase

--- test.php
+++ test.php
@@ -1,2 +1,14 @@
 <?php
+try {
+    $bdd = new PDO('mysql:host=92.222.93.228;dbname=dht11;charset=utf8', 'dht11', 'dht11');
+}
+catch(Exception $e) {
+    die('Erreur :' . $e->getMessage());
+}
+
+
 $config = include 'includes/config.inc';
+
+
+$reponse = $bdd->query("SELECT temperature, humidite FROM releves ORDER BY id DESC LIMIT 0, 1");
+
